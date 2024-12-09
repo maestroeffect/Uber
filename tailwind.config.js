504098fb76553c39da3192bb1f603ba413b94353
Precious <pomokhaiye8@gmail.com>
2024-12-09 12:34:45 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [require("nativewind/preset")], // Add this line
+};
